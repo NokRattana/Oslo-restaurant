8aed49a5b760412a63d69615ca368ed41d64571d
NokRattana <76916086+NokRattana@users.noreply.github.com>
2022-12-23 00:23:15 +0000

    Add files via upload

---
+++ review/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
+    #path('api/', include('api.urls')),
+    
+]
+
+
+
